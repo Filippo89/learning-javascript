e3a8546da716e85fa42c932358867656b899f044
piliffo <piliffo@computer-phil.(none)>
2013-03-05 20:35:28 +0100

    complete

--- 2013-03-05/esercizio3.js
+++ 2013-03-05/esercizio3.js
@@ -1,10 +1,12 @@
-for(var i=1; i<=10; i++){
+for(var i=1; i<=10; i++){ 
 	var s="";
 	for(var j=1; j<=10; j++){
-		if(i=j)
+		
+		if(j===i)
 			s+="1,"+"\t";
 		else
 			s+="0,"+"\t";
 	}
-	console.log(s);
+console.log(s);
 }
+
